9590ccdfbca90075c4d77b0d475ab4fcbcbc2fec
Joaquín Monteagudo Gómez <Kindos7@gmail.com>
2023-01-16 18:56:02 +0100

    Add test for add with carry instruction

--- src/instructions/arithmetic/tests.rs
+++ src/instructions/arithmetic/tests.rs
@@ -1,7 +1,7 @@
 use crate::hardware::cpu::Cpu;
 use crate::hardware::ram::{Ram, WORKING_RAM_START};
 use crate::hardware::register_bank::{DoubleRegisters, RegisterFlags, SingleRegisters};
-use crate::instructions::arithmetic::{ACC_REGISTER, Add, AddHl, AddImmediate, operation};
+use crate::instructions::arithmetic::{ACC_REGISTER, Add, AddHl, AddImmediate, AddWithCarry, operation};
 use crate::instructions::arithmetic::operation::ArithmeticOperation;
 use crate::instructions::Instruction;
 
@@ -51,4 +51,21 @@ fn add_immediate() {
 	assert!(!cpu.register_bank.read_bit_flag(RegisterFlags::Subtraction));
 	assert!(!cpu.register_bank.read_bit_flag(RegisterFlags::Carry));
 	assert!(!cpu.register_bank.read_bit_flag(RegisterFlags::HalfCarry));
+}
+
+#[test]
+fn add_with_carry() {
+	let mut cpu = Cpu::new();
+
+	cpu.register_bank.write_single_named(ACC_REGISTER, 0x80);
+	cpu.register_bank.write_single_named(SingleRegisters::B, 0x7F);
+	cpu.register_bank.write_bit_flag(RegisterFlags::Carry, true);
+
+	assert!(AddWithCarry::new(SingleRegisters::B).execute(&mut cpu).is_ok());
+	assert_eq!(cpu.register_bank.read_single_named(ACC_REGISTER), 0x00);
+	assert!(cpu.register_bank.read_bit_flag(RegisterFlags::Zero));
+	assert!(!cpu.register_bank.read_bit_flag(RegisterFlags::Subtraction));
+	assert!(cpu.register_bank.read_bit_flag(RegisterFlags::Carry));
+	assert!(cpu.register_bank.read_bit_flag(RegisterFlags::HalfCarry));
+
 }
