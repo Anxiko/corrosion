1432636208eb6f1551423883b293e15e13d5f818
Joaquín Monteagudo Gómez <Kindos7@gmail.com>
2023-01-16 22:53:51 +0100

    Implement xor instruction

--- src/instructions/logical.rs
+++ src/instructions/logical.rs
@@ -47,6 +47,28 @@ impl Instruction for Or {
 	}
 }
 
+pub(crate) struct Xor {
+	src: SingleRegisters,
+}
+
+impl Xor {
+	pub(crate) fn new(src: SingleRegisters) -> Self {
+		Self { src }
+	}
+}
+
+impl Instruction for Xor {
+	fn execute(&self, cpu: &mut Cpu) -> Result<(), ExecutionError> {
+		let dst_val = cpu.register_bank.read_single_named(ACC_REGISTER);
+		let src_val = cpu.register_bank.read_single_named(self.src);
+
+		let result = src_val ^ dst_val;
+		cpu.register_bank.write_single_named(ACC_REGISTER, result);
+
+		Ok(())
+	}
+}
+
 #[test]
 fn and_instruction() {
 	let mut cpu = Cpu::new();
@@ -70,5 +92,18 @@ fn or_instruction() {
 	expected.register_bank.write_single_named(ACC_REGISTER, 0b0111_1110);
 
 	assert!(Or::new(SingleRegisters::B).execute(&mut cpu).is_ok());
+	assert_eq!(cpu, expected);
+}
+
+#[test]
+fn xor_instruction() {
+	let mut cpu = Cpu::new();
+	cpu.register_bank.write_single_named(ACC_REGISTER, 0b0011_1100);
+	cpu.register_bank.write_single_named(SingleRegisters::B, 0b0101_1010);
+
+	let mut expected = cpu.clone();
+	expected.register_bank.write_single_named(ACC_REGISTER, 0b0110_0110);
+
+	assert!(Xor::new(SingleRegisters::B).execute(&mut cpu).is_ok());
 	assert_eq!(cpu, expected);
 }
