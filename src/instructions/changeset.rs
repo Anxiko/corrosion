8d72f56b05e7bbfc1f68807bee7ae5da80a748c7
Joaquín Monteagudo Gómez <Kindos7@gmail.com>
2023-04-04 01:20:24 +0200

    Move memory changes to submodule

--- src/instructions/changeset.rs
+++ src/instructions/changeset.rs
@@ -3,12 +3,10 @@ use std::fmt::Debug;
 use dyn_partial_eq::{dyn_partial_eq, DynPartialEq};
 
 use crate::hardware::cpu::Cpu;
-use crate::hardware::ram::Ram;
-use crate::hardware::register_bank::{DoubleRegisters, SingleRegisters};
 use crate::instructions::{ExecutionError, Instruction};
-use crate::instructions::base::double_byte::DoubleByteSource;
 
 pub(crate) use self::flags::{BitFlagsChange, ChangeIme};
+pub(crate) use self::memory::{MemoryByteWriteChange, MemoryDoubleByteWriteChange};
 pub(crate) use self::registers::{DoubleRegisterChange, SingleRegisterChange};
 pub(crate) use self::special_registers::{PcChange, SpChange};
 
@@ -21,81 +19,8 @@ mod registers;
 mod special_registers;
 mod flags;
 mod boxed;
+mod memory;
 
-
-#[derive(Debug, PartialEq)]
-enum MemoryByteWriteAddress {
-	Immediate(u16),
-	Register(DoubleRegisters),
-	OffsetRegister { base: u16, offset: SingleRegisters },
-}
-
-impl MemoryByteWriteAddress {
-	fn resolve(&self, cpu: &Cpu) -> u16 {
-		match self {
-			Self::Immediate(address) => *address,
-			Self::Register(double_register) => {
-				cpu.register_bank.read_double_named(*double_register)
-			}
-			Self::OffsetRegister { base, offset } => {
-				let offset_value = cpu.register_bank.read_single_named(*offset);
-				base.wrapping_add(offset_value.into())
-			}
-		}
-	}
-}
-
-#[derive(Debug, PartialEq, DynPartialEq)]
-pub(super) struct MemoryByteWriteChange {
-	address: MemoryByteWriteAddress,
-	value: u8,
-}
-
-impl MemoryByteWriteChange {
-	pub(super) fn write_to_immediate(address: u16, value: u8) -> Self {
-		Self { address: MemoryByteWriteAddress::Immediate(address), value }
-	}
-
-	pub(super) fn write_to_register(double_register: DoubleRegisters, value: u8) -> Self {
-		Self { address: MemoryByteWriteAddress::Register(double_register), value }
-	}
-
-	pub(super) fn write_to_offset(base: u16, offset: SingleRegisters, value: u8) -> Self {
-		Self { address: MemoryByteWriteAddress::OffsetRegister { base, offset }, value }
-	}
-}
-
-impl Change for MemoryByteWriteChange {
-	fn commit_change(&self, cpu: &mut Cpu) -> Result<(), ExecutionError> {
-		cpu.mapped_ram.write_byte(self.address.resolve(cpu), self.value)?;
-		Ok(())
-	}
-}
-
-#[derive(Debug, PartialEq, DynPartialEq)]
-pub(super) struct MemoryDoubleByteWriteChange {
-	address_source: DoubleByteSource,
-	value: u16,
-}
-
-impl MemoryDoubleByteWriteChange {
-	pub(super) fn new(address_source: DoubleByteSource, value: u16) -> Self {
-		Self { address_source, value }
-	}
-
-	pub(super) fn write_to_immediate_address(address: u16, value: u16) -> Self {
-		Self::new(DoubleByteSource::Immediate(address), value)
-	}
-}
-
-impl Change for MemoryDoubleByteWriteChange {
-	fn commit_change(&self, cpu: &mut Cpu) -> Result<(), ExecutionError> {
-		let address = self.address_source.read(cpu)?;
-
-		cpu.mapped_ram.write_double_byte(address, self.value)?;
-		Ok(())
-	}
-}
 
 #[derive(PartialEq, DynPartialEq, Debug)]
 pub(crate) struct ChangeList {
---
+++ src/instructions/changeset/memory.rs
@@ -0,0 +1,82 @@
+use dyn_partial_eq::DynPartialEq;
+
+use crate::hardware::cpu::Cpu;
+use crate::hardware::ram::Ram;
+use crate::hardware::register_bank::{DoubleRegisters, SingleRegisters};
+use crate::instructions::base::double_byte::DoubleByteSource;
+use crate::instructions::changeset::Change;
+use crate::instructions::ExecutionError;
+
+#[derive(Debug, PartialEq)]
+enum MemoryByteWriteAddress {
+	Immediate(u16),
+	Register(DoubleRegisters),
+	OffsetRegister { base: u16, offset: SingleRegisters },
+}
+
+impl MemoryByteWriteAddress {
+	fn resolve(&self, cpu: &Cpu) -> u16 {
+		match self {
+			Self::Immediate(address) => *address,
+			Self::Register(double_register) => {
+				cpu.register_bank.read_double_named(*double_register)
+			}
+			Self::OffsetRegister { base, offset } => {
+				let offset_value = cpu.register_bank.read_single_named(*offset);
+				base.wrapping_add(offset_value.into())
+			}
+		}
+	}
+}
+
+#[derive(Debug, PartialEq, DynPartialEq)]
+pub(crate) struct MemoryByteWriteChange {
+	address: MemoryByteWriteAddress,
+	value: u8,
+}
+
+impl MemoryByteWriteChange {
+	pub(crate) fn write_to_immediate(address: u16, value: u8) -> Self {
+		Self { address: MemoryByteWriteAddress::Immediate(address), value }
+	}
+
+	pub(crate) fn write_to_register(double_register: DoubleRegisters, value: u8) -> Self {
+		Self { address: MemoryByteWriteAddress::Register(double_register), value }
+	}
+
+	pub(crate) fn write_to_offset(base: u16, offset: SingleRegisters, value: u8) -> Self {
+		Self { address: MemoryByteWriteAddress::OffsetRegister { base, offset }, value }
+	}
+}
+
+impl Change for MemoryByteWriteChange {
+	fn commit_change(&self, cpu: &mut Cpu) -> Result<(), ExecutionError> {
+		cpu.mapped_ram.write_byte(self.address.resolve(cpu), self.value)?;
+		Ok(())
+	}
+}
+
+#[derive(Debug, PartialEq, DynPartialEq)]
+pub(crate) struct MemoryDoubleByteWriteChange {
+	address_source: DoubleByteSource,
+	value: u16,
+}
+
+impl MemoryDoubleByteWriteChange {
+	pub(crate) fn new(address_source: DoubleByteSource, value: u16) -> Self {
+		Self { address_source, value }
+	}
+
+	pub(crate) fn write_to_immediate_address(address: u16, value: u16) -> Self {
+		Self::new(DoubleByteSource::Immediate(address), value)
+	}
+}
+
+impl Change for MemoryDoubleByteWriteChange {
+	fn commit_change(&self, cpu: &mut Cpu) -> Result<(), ExecutionError> {
+		let address = self.address_source.read(cpu)?;
+
+		cpu.mapped_ram.write_double_byte(address, self.value)?;
+		Ok(())
+	}
+}
