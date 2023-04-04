49d746543e7be20ad529be69beacf1f39862d4e9
Joaquín Monteagudo Gómez <Kindos7@gmail.com>
2023-04-05 00:54:13 +0200

    Add test for noop

--- src/instructions/changeset/noop.rs
+++ src/instructions/changeset/noop.rs
@@ -1,6 +1,8 @@
 use dyn_partial_eq::DynPartialEq;
+
 use crate::hardware::cpu::Cpu;
 use crate::instructions::ExecutionError;
+
 use super::Change;
 
 #[derive(PartialEq, DynPartialEq, Debug)]
@@ -15,5 +17,19 @@ impl NoChange {
 impl Change for NoChange {
 	fn commit_change(&self, _cpu: &mut Cpu) -> Result<(), ExecutionError> {
 		Ok(())
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn noop() {
+		let mut actual = Cpu::new();
+		let expected = actual.clone();
+
+		NoChange::new().commit_change(&mut actual).unwrap();
+		assert_eq!(actual, expected);
 	}
 }
