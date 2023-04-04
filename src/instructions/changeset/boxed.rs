20ab88650699e858ec3f8f402001dba6965e82cb
Joaquín Monteagudo Gómez <Kindos7@gmail.com>
2023-04-05 01:00:05 +0200

    Add tests for boxed change

--- src/instructions/changeset/boxed.rs
+++ src/instructions/changeset/boxed.rs
@@ -29,3 +29,40 @@ impl Change for Box<dyn Change> {
 		boxed_change.commit_change(cpu)
 	}
 }
+
+#[cfg(test)]
+mod tests {
+	use crate::hardware::register_bank::DoubleRegisters;
+	use crate::instructions::ACC_REGISTER;
+	use crate::instructions::changeset::{DoubleRegisterChange, SingleRegisterChange};
+
+	use super::*;
+
+	#[test]
+	fn equality() {
+		let left: Box<dyn Change> = Box::new(SingleRegisterChange::new(ACC_REGISTER, 0x12));
+		let right: Box<dyn Change> = Box::new(SingleRegisterChange::new(ACC_REGISTER, 0x12));
+
+		assert_eq!(left, right)
+	}
+
+	#[test]
+	fn inequality() {
+		let left: Box<dyn Change> = Box::new(SingleRegisterChange::new(ACC_REGISTER, 0x12));
+		let right: Box<dyn Change> = Box::new(DoubleRegisterChange::new(DoubleRegisters::HL, 0x12));
+
+		assert_ne!(left, right)
+	}
+
+	#[test]
+	fn change() {
+		let mut actual = Cpu::new();
+		let mut expected = actual.clone();
+		expected.register_bank.write_single_named(ACC_REGISTER, 0x12);
+
+		let change: Box<dyn Change> = Box::new(SingleRegisterChange::new(ACC_REGISTER, 0x12));
+		change.commit_change(&mut actual).unwrap();
+
+		assert_eq!(actual, expected);
+	}
+}
