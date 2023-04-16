08d16d3f3a2171ac11ca9decc2c1119a47fd64dd
Joaquín Monteagudo Gómez <Kindos7@gmail.com>
2023-04-16 19:23:59 +0200

    Add test for branch conditions

--- src/instructions/flow/condition.rs
+++ src/instructions/flow/condition.rs
@@ -1,17 +1,10 @@
 use crate::hardware::cpu::Cpu;
 use crate::hardware::register_bank::BitFlags;
-use crate::instructions::base::double_byte::DoubleByteSource;
 
 #[derive(Copy, Clone, PartialEq, Debug)]
 pub(crate) enum BranchCondition {
 	Unconditional,
 	TestFlag { flag: BitFlags, branch_if_equals: bool },
-}
-
-#[derive(Copy, Clone, PartialEq, Debug)]
-pub(crate) enum JumpInstructionDestination {
-	FromSource(DoubleByteSource),
-	RelativeToPc(i8),
 }
 
 impl BranchCondition {
@@ -22,5 +15,37 @@ impl BranchCondition {
 				cpu.register_bank.read_bit_flag(*flag) == *branch_if_equals
 			}
 		}
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use crate::hardware::cpu::Cpu;
+	use crate::hardware::register_bank::BitFlags;
+	use crate::instructions::flow::BranchCondition;
+
+	#[test]
+	fn unconditional() {
+		let cpu = Cpu::new();
+
+		assert!(BranchCondition::Unconditional.satisfied(&cpu))
+	}
+
+	#[test]
+	fn satisfied_flag_test() {
+		let mut cpu = Cpu::new();
+		cpu.register_bank.write_bit_flag(BitFlags::Carry, true);
+
+		assert!(BranchCondition::TestFlag { flag: BitFlags::Carry, branch_if_equals: true }.satisfied(&cpu));
+		assert!(BranchCondition::TestFlag { flag: BitFlags::Zero, branch_if_equals: false }.satisfied(&cpu));
+	}
+
+	#[test]
+	fn unsatisfied_flag_test() {
+		let mut cpu = Cpu::new();
+		cpu.register_bank.write_bit_flag(BitFlags::Carry, true);
+
+		assert!(!BranchCondition::TestFlag { flag: BitFlags::Carry, branch_if_equals: false }.satisfied(&cpu));
+		assert!(!BranchCondition::TestFlag { flag: BitFlags::Zero, branch_if_equals: true }.satisfied(&cpu));
 	}
 }
