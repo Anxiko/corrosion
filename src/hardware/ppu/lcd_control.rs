54ed606a9f720b07d1e74517958bc3178e01ba81
Joaquín Monteagudo Gómez <Kindos7@gmail.com>
2023-04-25 20:48:55 +0200

    Decode all the LCD control bits

--- src/hardware/ppu/lcd_control.rs
+++ src/hardware/ppu/lcd_control.rs
@@ -4,17 +4,48 @@ const DISPLAY_ENABLED_BIT: usize = 7;
 const TILE_SOURCE_BIT: usize = 4;
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
-pub(super) enum TileSource {
-	/* The addressing mode used for BG and Window */
+pub(super) enum TileDataAddressMode {
+	/* The addressing mode used by BG and Window for tile data*/
 	SignedAddressing,   // 0x8800 to 0x97FF
 	UnsignedAddressing, // 0x8000 to 0x8FFF
 }
 
-impl From<bool> for TileSource {
+impl From<bool> for TileDataAddressMode {
 	fn from(value: bool) -> Self {
 		match value {
-			false => TileSource::SignedAddressing,
-			true => TileSource::UnsignedAddressing,
+			false => Self::SignedAddressing,
+			true => Self::UnsignedAddressing,
+		}
+	}
+}
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+pub(super) enum TileMapAddressMode {
+	/* The addressing mode used by BG and Window for the tile map */
+	First,  // 0x9800 to 0x9BFF
+	Second, // 0x9C00 to 0x9FFF
+}
+
+impl From<bool> for TileMapAddressMode {
+	fn from(value: bool) -> Self {
+		match value {
+			false => Self::First,
+			true => Self::Second,
+		}
+	}
+}
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+pub(super) enum ObjSize {
+	SingleTile,
+	DoubleTile,
+}
+
+impl From<bool> for ObjSize {
+	fn from(value: bool) -> Self {
+		match value {
+			false => Self::SingleTile,
+			true => Self::DoubleTile,
 		}
 	}
 }
@@ -22,15 +53,29 @@ impl From<bool> for TileSource {
 #[derive(Debug, Eq, PartialEq)]
 pub(super) struct DecodedLcdControl {
 	pub display_enabled: bool,
-	pub tile_source: TileSource,
+	pub window_tile_map: TileMapAddressMode,
+	pub window_enable: bool,
+	pub tile_data: TileDataAddressMode,
+	pub bg_tile_map: TileMapAddressMode,
+	pub obj_size: ObjSize,
+	pub obj_enable: bool,
+	pub bg_and_window_priority: bool,
 }
 
 impl From<u8> for DecodedLcdControl {
 	fn from(value: u8) -> Self {
-		let bits = byte_to_bits(value);
+		let [bg_and_window_priority, obj_enable, obj_size, bg_tile_map, tile_data, window_enable, window_tile_map, display_enabled] =
+			byte_to_bits(value);
+
 		Self {
-			display_enabled: bits[DISPLAY_ENABLED_BIT],
-			tile_source: TileSource::from(bits[TILE_SOURCE_BIT]),
+			display_enabled,
+			window_tile_map: window_tile_map.into(),
+			window_enable,
+			tile_data: tile_data.into(),
+			bg_tile_map: bg_tile_map.into(),
+			obj_size: obj_size.into(),
+			obj_enable,
+			bg_and_window_priority,
 		}
 	}
 }
@@ -46,7 +91,13 @@ mod tests {
 		let actual = DecodedLcdControl::from(LCD_CONTROL);
 		let expected = DecodedLcdControl {
 			display_enabled: false,
-			tile_source: TileSource::UnsignedAddressing,
+			window_tile_map: TileMapAddressMode::First,
+			window_enable: true,
+			tile_data: TileDataAddressMode::UnsignedAddressing,
+			bg_tile_map: TileMapAddressMode::First,
+			obj_size: ObjSize::DoubleTile,
+			obj_enable: false,
+			bg_and_window_priority: true,
 		};
 
 		assert_eq!(actual, expected);
@@ -57,7 +108,13 @@ mod tests {
 		let actual = DecodedLcdControl::from(!LCD_CONTROL);
 		let expected = DecodedLcdControl {
 			display_enabled: true,
-			tile_source: TileSource::SignedAddressing,
+			window_tile_map: TileMapAddressMode::Second,
+			window_enable: false,
+			tile_data: TileDataAddressMode::SignedAddressing,
+			bg_tile_map: TileMapAddressMode::Second,
+			obj_size: ObjSize::SingleTile,
+			obj_enable: true,
+			bg_and_window_priority: false,
 		};
 
 		assert_eq!(actual, expected);
